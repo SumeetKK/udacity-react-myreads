d175a6e317ae9ef4eb147ad51a62ca85612c1433
Sumeet Kumar <sumeet@evolvingtech.pk>
2020-05-17 02:52:45 +0500

    Converted SearchedBooks to functional component

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,29 +1,28 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types';
 import Book from './Book'
 
-class SearchBooks extends Component{
-    
-  update = this.props.update;
-  render(){
-    return <div className="search-books">
-      <div className="search-books-bar">
-        <Link to="/"><button className="close-search">Close</button></Link>
-        <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author" onChange={this.props.search} />
-        </div>
-      </div>
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {(!this.props.searchedBooks.error) && this.props.searchedBooks.map((book) =>
-                <li key={book.id}><Book book={book} update={this.update}/></li>
-            )
-          }
-        </ol>
+const SearchBooks = (props) =>
+{
+  const update = props.update;
+
+  return <div className="search-books">
+    <div className="search-books-bar">
+      <Link to="/"><button className="close-search">Close</button></Link>
+      <div className="search-books-input-wrapper">
+        <input type="text" placeholder="Search by title or author" onChange={props.search} />
       </div>
     </div>
-  }
+    <div className="search-books-results">
+      <ol className="books-grid">
+        {(!props.searchedBooks.error) && props.searchedBooks.map((book) =>
+              <li key={book.id}><Book book={book} update={update}/></li>
+          )
+        }
+      </ol>
+    </div>
+  </div>
 }
 
 SearchBooks.propTypes = {
