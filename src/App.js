fea29ab78d47766f73682e80e1c5f2c30d3a09ee
Sumeet Kumar <sumeet@evolvingtech.pk>
2020-05-17 02:53:41 +0500

    Fixed Search Bugs and Added 404 Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
+import Error404 from './Error404'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -35,23 +36,33 @@ class BooksApp extends React.Component {
   }
 
   search = (event) => {
+    console.log(event.target.value);
     (event.target.value) && BooksAPI.search(event.target.value).then((searchedBooks) => {
-      for(let book of searchedBooks)
+      //Preventing the Error
+      if(searchedBooks.length !== undefined)
       {
-        if(this.state.books.length < 1)
+        for(let book of searchedBooks)
         {
-          this.getBooks();
-        }
-        for(let shelvedBook of this.state.books)
-        {
-          if(book.id === shelvedBook.id)
+          if(this.state.books.length < 1)
           {
-            book.shelf = shelvedBook.shelf;
+            this.getBooks();
+          }
+          for(let shelvedBook of this.state.books)
+          {
+            if(book.id === shelvedBook.id)
+            {
+              book.shelf = shelvedBook.shelf;
+            }
           }
         }
       }
       this.setState({searchedBooks})
     })
+
+    if(event.target.value === '')
+    {
+      this.setState({searchedBooks: []})
+    }
   }
 
   toggleSearch = () => {
@@ -62,23 +73,26 @@ class BooksApp extends React.Component {
     return (
       <Router>
         <div className="app">
-          <Route exact path="/search">
-            <SearchBooks 
-              search = {this.search}
-              searchedBooks = {this.state.searchedBooks}
-              toggleSearch = {this.toggleSearch}
-              update = {this.update}
-            />
-          </Route>
-          <Route exact path="/">
-            <ListBooks 
-              currentlyReading = {this.state.currentlyReading}
-              wantToRead = {this.state.wantToRead}
-              read = {this.state.read}
-              update={this.update}
-              toggleSearch = {this.toggleSearch}
-            />
-          </Route>
+          <Switch>
+            <Route exact path="/search">
+              <SearchBooks 
+                search = {this.search}
+                searchedBooks = {this.state.searchedBooks}
+                toggleSearch = {this.toggleSearch}
+                update = {this.update}
+              />
+            </Route>
+            <Route exact path="/">
+              <ListBooks 
+                currentlyReading = {this.state.currentlyReading}
+                wantToRead = {this.state.wantToRead}
+                read = {this.state.read}
+                update={this.update}
+                toggleSearch = {this.toggleSearch}
+              />
+            </Route>
+            <Route component={Error404} />
+          </Switch>
         </div>
       </Router>
     )
